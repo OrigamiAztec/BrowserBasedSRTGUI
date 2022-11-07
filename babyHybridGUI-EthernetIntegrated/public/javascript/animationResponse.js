2f5167ae49f78e9d68b4782c1495a8aed71840f6
Antonio <origamiaztec@gmail.com>
2022-11-06 23:24:04 -0600

    updated data recording status and added GUI that integrates with ethernet
    csv file will push button selection by date, time, and command sent. command sent should probably be sent from a dictionary with command that corresponds to what's clicked.
    
    IntegratedGUI worked to send and read commands in real time from launchbox from first failed baby hybrid fire where engine depressurized too quickly.

--- babyHybridGUI-EthernetIntegrated/public/javascript/animationResponse.js
+++ babyHybridGUI-EthernetIntegrated/public/javascript/animationResponse.js
@@ -10,10 +10,33 @@ var sysArmStatus = 0;
 var solArmStatus = 0;
 var ignArmStatus = 0;
 
+var runTankWeight;
+var supplyTankWeight;
+
 var DataRecordingStatus = 0;
 
 var globalTime;
 var globalDate;
+
+var lastReceivedString;
+
+//logging all data received from ethernet port over console:
+var socket = io();
+
+socket.on('data2', function(data) {        
+        console.log(data);
+        if (data.includes("z/")){
+            document.querySelector('.last_received_text').textContent = "Last Received: " + data.split("/")[1];
+        }
+});
+
+socket.on('padWeightdata', function(data){
+    //runTankWeight = DataRecordingStatus
+    runTankWeight = parseFloat(data.split(",")[0]);
+    supplyTankWeight = parseFloat(data.split(",")[1]);
+    //console.log(runTankWeight, supplyTankWeight);
+    document.querySelector(".LC2Label2").textContent = runTankWeight + " lbf";
+});
 
 //top navigation bar functions:
 function openCloseRightNavBar(){
@@ -33,7 +56,8 @@ function armDisarmSystem(){
         sysArmStatus ++;
         console.log("arming system");
         //console.log(globalTime);
-        document.querySelector('.last_sent_text').textContent = " Last Sent: SYS ARM";
+        socket.emit('inputString', "d");
+        document.querySelector('.last_sent_text').textContent = "Last Sent: SYS ARM";
         document.querySelector('.knob-sysArm').style.transform = 'translate(5em)';
         document.querySelector('.knob-sysArm').style["background-color"] = '#6ade6c';
         document.querySelector('.sysArmButton').style["background-color"] = "rgb(0, 128, 20)";
@@ -46,13 +70,40 @@ function armDisarmSystem(){
     }
     else{
         sysArmStatus = 0;
+        solArmStatus = 0;
+        ignArmStatus = 0;
+
         console.log("disarming system");
+        socket.emit('inputString', "e");
+
+        console.log("disarming solenoid");
+        socket.emit('inputString', "g");
+
+        console.log("disarming Igniter");
+        socket.emit('inputString', "i");
+
+        //update system arming button
         document.querySelector('.last_sent_text').textContent = " Last Sent: SYS DISARM";
         document.querySelector('.knob-sysArm').style.transform = 'translate(0px)';
         document.querySelector('.knob-sysArm').style["background-color"] = '#a30b00';
         document.querySelector('.knob-sysLabel').textContent = "SYS DISARMED";  
         document.querySelector('.knob-sysLabel').style["color"] = "white";  
         document.querySelector('.sysArmButton').style["background-color"] = "#cf8580";
+
+        //update solenoid arming button
+        document.querySelector('.knob-solArm').style.transform = 'translate(0px)';
+        document.querySelector('.knob-solArm').style["background-color"] = '#a30b00';
+        document.querySelector('.knob-solLabel').textContent = "SOL DISARMED";  
+        document.querySelector('.knob-solLabel').style["color"] = "white";  
+        document.querySelector('.solArmButton').style["background-color"] = "#cf8580";
+
+        //update ignition arming button
+        document.querySelector('.knob-ignArm').style.transform = 'translate(0px)';
+        document.querySelector('.knob-ignArm').style["background-color"] = '#a30b00';
+        document.querySelector('.knob-ignLabel').textContent = "IGN DISARMED";  
+        document.querySelector('.knob-ignLabel').style["color"] = "white";  
+        document.querySelector('.ignArmButton').style["background-color"] = "#cf8580";
+        
         if (DataRecordingStatus){
             csvSimulatedFileData.push([globalDate, globalTime, "SYS DISARM"]);
         }
@@ -63,23 +114,31 @@ function armDisarmSol(){
     if (sysArmStatus && (solArmStatus == 0)){
         solArmStatus ++;
         console.log("arming solenoid");
+        socket.emit('inputString', "f");
         document.querySelector('.last_sent_text').textContent = " Last Sent: SOL ARM";
         document.querySelector('.knob-solArm').style.transform = 'translate(5em)';
         document.querySelector('.knob-solArm').style["background-color"] = '#6ade6c';
         document.querySelector('.solArmButton').style["background-color"] = "rgb(0, 128, 20)";
         document.querySelector('.knob-solLabel').textContent = "SOL ARMED";  
         document.querySelector('.knob-solLabel').style["color"] = "black";  
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "SOL ARM"]);
+        }  
 
     }
     else{
         solArmStatus = 0;
         console.log("disarming solenoid");
+        socket.emit('inputString', "g");
         document.querySelector('.last_sent_text').textContent = " Last Sent: SOL DISARM";
         document.querySelector('.knob-solArm').style.transform = 'translate(0px)';
         document.querySelector('.knob-solArm').style["background-color"] = '#a30b00';
         document.querySelector('.knob-solLabel').textContent = "SOL DISARMED";  
         document.querySelector('.knob-solLabel').style["color"] = "white";  
         document.querySelector('.solArmButton').style["background-color"] = "#cf8580";
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "SOL DISARM"]);
+        }  
     }
 }
 
@@ -87,23 +146,31 @@ function armDisarmIgn(){
     if (sysArmStatus && (ignArmStatus == 0)){
         ignArmStatus ++;
         console.log("arming Igniter");
+        socket.emit('inputString', "h");
         document.querySelector('.last_sent_text').textContent = " Last Sent: IGN ARM";
         document.querySelector('.knob-ignArm').style.transform = 'translate(5em)';
         document.querySelector('.knob-ignArm').style["background-color"] = '#6ade6c';
         document.querySelector('.ignArmButton').style["background-color"] = "rgb(0, 128, 20)";
         document.querySelector('.knob-ignLabel').textContent = "IGN ARMED";  
         document.querySelector('.knob-ignLabel').style["color"] = "black";  
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "IGN ARM"]);
+        }  
 
     }
     else{
         ignArmStatus = 0;
         console.log("disarming Igniter");
+        socket.emit('inputString', "i");
         document.querySelector('.last_sent_text').textContent = " Last Sent: IGN DISARM";
         document.querySelector('.knob-ignArm').style.transform = 'translate(0px)';
         document.querySelector('.knob-ignArm').style["background-color"] = '#a30b00';
         document.querySelector('.knob-ignLabel').textContent = "IGN DISARMED";  
         document.querySelector('.knob-ignLabel').style["color"] = "white";  
         document.querySelector('.ignArmButton').style["background-color"] = "#cf8580";
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "IGN DISARM"]);
+        }  
     }
 }
 
@@ -111,22 +178,30 @@ function switchIgnitionButton(){
     if (ignArmStatus && (ignitionInput == 0)){
         ignitionInput ++;
         console.log("ignition on");
+        socket.emit('inputString', "n");
         document.querySelector('.last_sent_text').textContent = " Last Sent: IGN+";
         document.querySelector('.knob').style.transform = 'translate(40px)';
         document.querySelector('.knob').style["background-color"] = '#6ade6c';
         document.querySelector('.knob').textContent = "ON";  
         document.querySelector('.ignitionButton').style["background-color"] = '#008014';
         document.querySelector('.pipeUp4').style["boxShadow"] = " 0 2px 2px 2px white";
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "IGN ON"]);
+        }  
     }
     else{
         ignitionInput = 0;
         console.log("ignition off");
+        socket.emit('inputString', "o");
         document.querySelector('.last_sent_text').textContent = " Last Sent: IGN-";
         document.querySelector('.knob').style.transform = 'translate(0px)';
         document.querySelector('.knob').style["background-color"] = '#a30b00';
         document.querySelector('.knob').textContent = "OFF";  
         document.querySelector('.ignitionButton').style["background-color"] = '#cf8580';
         document.querySelector('.pipeUp4').style["boxShadow"] = " 0 0 0 0 white";
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "IGN OFF"]);
+        }  
     }
 
 }
@@ -137,15 +212,23 @@ function rotateValveLine3(){
     if (solArmStatus && (valveInput3 == 0)){
         valveInput3 ++;
         console.log("supply vent on");
+        socket.emit('inputString', "l");
         document.querySelector('.last_sent_text').textContent = " Last Sent: OV2+";
         document.querySelector('.valvePosInput3').style.transform = 'rotate(90deg)';
         document.querySelector('.pipeUp2').style["boxShadow"] = " 0 2px 2px 2px white";
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "OV2 OPEN"]);
+        }  
     }else{
         valveInput3 = 0;
-        console.log("supply vent off")
+        console.log("supply vent off");
+        socket.emit('inputString', "m");
         document.querySelector('.last_sent_text').textContent = " Last Sent: OV2-";
         document.querySelector('.valvePosInput3').style.transform = 'rotate(45deg)';
         document.querySelector('.pipeUp2').style["boxShadow"] = " 0 0 0 0 white";
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "OV2 CLOSE"]);
+        }  
     }
 
     document.querySelector('.responseIndicator3').style["boxShadow"] = "0 0 10px 5px red";
@@ -161,22 +244,30 @@ function rotateValveLine2(){
     if (solArmStatus && (valveInput2 == 0)){
         valveInput2 ++;
         console.log("supply fill on");
+        socket.emit('inputString', "j");
         document.querySelector('.last_sent_text').textContent = " Last Sent: OV1+";
         document.querySelector('.valvePosInput2').style.transform = 'rotate(0deg)';
         document.querySelector('.pipeRight').style["boxShadow"] = " 0 0 2px 2px white";
         document.querySelector('.pipeUp1').style["boxShadow"] = " 0 2px 2px 2px white";
         document.querySelector('.pipeUp3').style["boxShadow"] = " 0 2px 2px 2px white";
         document.querySelector('.pipeUp6').style["boxShadow"] = " 0 2px 2px 2px white";
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "OV1 OPEN"]);
+        }  
     }
     else{
         valveInput2 = 0;
         console.log("supply fill off");
+        socket.emit('inputString', "k");
         document.querySelector('.last_sent_text').textContent = " Last Sent: OV1-";
         document.querySelector('.valvePosInput2').style.transform = 'rotate(45deg)';
         document.querySelector('.pipeRight').style["boxShadow"] = " 0 0 0 0 white";
         document.querySelector('.pipeUp1').style["boxShadow"] = " 0 0 0 0 white";
         document.querySelector('.pipeUp3').style["boxShadow"] = " 0 0 0 0 white";
         document.querySelector('.pipeUp6').style["boxShadow"] = " 0 0 0 0 white";
+        if (DataRecordingStatus){
+            csvSimulatedFileData.push([globalDate, globalTime, "OV1 CLOSE"]);
+        }  
     }
 
     document.querySelector('.responseIndicator2').style["boxShadow"] = "0 0 10px 5px red";
@@ -228,7 +319,22 @@ function updateNavDate() {
 }
 updateNavDate(); // initial call
 
+/*
+function updateRunTankWeight(){
+    socket.on('padWeightdata', function(data){
+        //runTankWeight = DataRecordingStatus
+        runTankWeight = parseFloat(data.split(",")[0]);
+        supplyTankWeight = parseFloat(data.split(",")[1]);
+        //console.log(runTankWeight, supplyTankWeight);
+        document.querySelector(".LC2Label2").textContent = runTankWeight + " lbf";
+    });
+    setTimeout(updateRunTankWeight, 1000);
+}
+// inital call to updateRunTankWeight
+updateRunTankWeight();*/
+
 // outputting simulated data for plotting
+/*
 function simPressureData(){
     var guessVal = Math.random() * (1200 - 800) + 800;
     return guessVal;
@@ -312,20 +418,20 @@ setInterval(function(){
     }
 
 }, 100); 
-
+*/
 
 //section below will set response to starting to record data when clicked
 function updateDataRecordingState(){
     if (DataRecordingStatus == 0){
         DataRecordingStatus ++;
         console.log("recording data");
-        document.getElementById('imgClickAndChange').src = 'public/images/playButtonLogoRed.PNG';
+        document.getElementById('imgClickAndChange').src = '/images/playButtonLogoRed.PNG';
         document.getElementById('recordingTextStatus').textContent = "Recording";
     }
     else{
         DataRecordingStatus = 0;
         console.log("not recording data");
-        document.getElementById('imgClickAndChange').src = 'public/images/playButtonLogo.PNG';
+        document.getElementById('imgClickAndChange').src = '/images/playButtonLogo.PNG';
         document.getElementById('recordingTextStatus').textContent = "Not Recording";
     }
 }
